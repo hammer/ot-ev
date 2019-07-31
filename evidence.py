bfbfee366dd3efe6b84e4571679517106a765826
Jeff Hammerbacher <jeff.hammerbacher@gmail.com>
2019-07-31 14:45:00 -0400

    Can now score all 140 evidence examples!

--- evidence.py
+++ evidence.py
@@ -1,4 +1,80 @@
+#!/usr/bin/env python3  
 import json
+import jsonlines
+import math
+from past.utils import old_div
+
+# mrtarget.data.19.06.yml
+datasources_to_datatypes = {
+    'cancer_gene_census': 'somatic_mutation',
+    'chembl': 'known_drug',
+    'crispr': 'affected_pathway',
+    'europepmc': 'literature',
+    'eva': 'genetic_association',
+    'eva_somatic': 'somatic_mutation',
+    'expression_atlas': 'rna_expression',
+    'gene2phenotype': 'genetic_association',
+    'genomics_england': 'genetic_association',
+    'gwas_catalog': 'genetic_association',
+    'intogen': 'somatic_mutation',
+    'phenodigm': 'animal_model',
+    'phewas_catalog': 'genetic_association',
+    'postgap': 'genetic_association',
+    'progeny': 'affected_pathway',
+    'reactome': 'affected_pathway',
+    'slapenrich': 'affected_pathway',
+    'sysbio': 'affected_pathway',
+    'uniprot': 'genetic_association',
+    'uniprot_literature': 'genetic_association',
+    'uniprot_somatic': 'somatic_mutation',
+}
+
+# https://raw.githubusercontent.com/opentargets/data_pipeline/master/mrtarget/resources/eco_scores.tsv
+eco_scores = {
+    'http://identifiers.org/eco/cttv_mapping_pipeline': 1.0,
+    'http://purl.obolibrary.org/obo/ECO_0000205': 1.0,
+    'http://purl.obolibrary.org/obo/SO_0002165': 1.0,
+    'http://purl.obolibrary.org/obo/SO_0001060': 0.5,
+    'http://purl.obolibrary.org/obo/SO_0001566': 0.6,
+    'http://purl.obolibrary.org/obo/SO_0001567': 0.65,
+    'http://purl.obolibrary.org/obo/SO_0001574': 0.95,
+    'http://purl.obolibrary.org/obo/SO_0001575': 0.95,
+    'http://purl.obolibrary.org/obo/SO_0001578': 0.9,
+    'http://purl.obolibrary.org/obo/SO_0001580': 0.95,
+    'http://purl.obolibrary.org/obo/SO_0001582': 0.7,
+    'http://purl.obolibrary.org/obo/SO_0001583': 0.7,
+    'http://purl.obolibrary.org/obo/SO_0001587': 0.95,
+    'http://purl.obolibrary.org/obo/SO_0001589': 0.95,
+    'http://purl.obolibrary.org/obo/SO_0001619': 0.65,
+    'http://purl.obolibrary.org/obo/SO_0001620': 0.65,
+    'http://purl.obolibrary.org/obo/SO_0001621': 0.65,
+    'http://purl.obolibrary.org/obo/SO_0001623': 0.65,
+    'http://purl.obolibrary.org/obo/SO_0001624': 0.65,
+    'http://purl.obolibrary.org/obo/SO_0001626': 0.9,
+    'http://purl.obolibrary.org/obo/SO_0001627': 0.65,
+    'http://purl.obolibrary.org/obo/SO_0001628': 0.5,
+    'http://purl.obolibrary.org/obo/SO_0001630': 0.95,
+    'http://purl.obolibrary.org/obo/SO_0001631': 0.6,
+    'http://purl.obolibrary.org/obo/SO_0001632': 0.6,
+    'http://purl.obolibrary.org/obo/SO_0001782': 0.6,
+    'http://purl.obolibrary.org/obo/SO_0001792': 0.65,
+    'http://purl.obolibrary.org/obo/SO_0001818': 0.7,
+    'http://purl.obolibrary.org/obo/SO_0001819': 0.65,
+    'http://purl.obolibrary.org/obo/SO_0001821': 0.7,
+    'http://purl.obolibrary.org/obo/SO_0001822': 0.7,
+    'http://purl.obolibrary.org/obo/SO_0001825': 0.5,
+    'http://purl.obolibrary.org/obo/SO_0001889': 0.6,
+    'http://purl.obolibrary.org/obo/SO_0001891': 0.6,
+    'http://purl.obolibrary.org/obo/SO_0001892': 0.6,
+    'http://purl.obolibrary.org/obo/SO_0001893': 1.0,
+    'http://purl.obolibrary.org/obo/SO_0001894': 0.6,
+    'http://purl.obolibrary.org/obo/SO_0001895': 0.6,
+    'http://purl.obolibrary.org/obo/SO_0001906': 0.6,
+    'http://purl.obolibrary.org/obo/SO_0001907': 0.6,
+    'http://purl.obolibrary.org/obo/SO_0002012': 0.95,
+    'http://targetvalidation.org/sequence/nearest_gene_five_prime_end': 0.5,
+    'http://targetvalidation.org/sequence/regulatory_nearest_gene_five_prime_end': 0.6,
+}
 
 class PipelineEncoder(json.JSONEncoder):
     def default(self, o):
@@ -72,28 +148,6 @@ class Evidence(JSONSerializable):
             self.load_json(evidence)
             
         self.datasource = self.evidence['sourceID']
-        datasources_to_datatypes = {
-            'cancer_gene_census': 'somatic_mutation',
-            'chembl': 'known_drug',
-            'crispr': 'affected_pathway',
-            'europepmc': 'literature',
-            'eva': 'genetic_association',
-            'eva_somatic': 'somatic_mutation',
-            'expression_atlas': 'rna_expression',
-            'gene2phenotype': 'genetic_association',
-            'genomics_england': 'genetic_association',
-            'gwas_catalog': 'genetic_association',
-            'intogen': 'somatic_mutation',
-            'phenodigm': 'animal_model',
-            'phewas_catalog': 'genetic_association',
-            'postgap': 'genetic_association',
-            'progeny': 'affected_pathway',
-            'reactome': 'affected_pathway',
-            'slapenrich': 'affected_pathway',
-            'sysbio': 'affected_pathway',
-            'uniprot': 'genetic_association',
-            'uniprot_literature': 'genetic_association',
-            'uniprot_somatic': 'somatic_mutation'}
         self.datatype = datasources_to_datatypes[self.datasource]
 
     def get_id(self):
@@ -246,3 +300,133 @@ class Evidence(JSONSerializable):
         normalised_no_of_cases = DataNormaliser.renormalize(no_of_cases, [0, max_cases], [0, 1])
         score = normalised_pvalue * normalised_no_of_cases
         return score
+
+def fix_evidence(evidence):
+    evidence = evidence.evidence
+    fixed = False
+
+    # fix errors in data here so nobody needs to ask corrections to the data provider
+    # fix missing version in gwas catalog data
+    if 'variant2disease' in evidence:
+        try:
+            float(evidence['evidence']['variant2disease']['provenance_type']['database']['version'])
+        except:
+            evidence['evidence']['variant2disease']['provenance_type']['database']['version'] = ''
+            fixed = True
+        try:
+            float(evidence['evidence']['variant2disease']['provenance_type']['database']['dbxref']['version'])
+        except:
+            evidence['evidence']['variant2disease']['provenance_type']['database']['dbxref']['version'] = ''
+            fixed = True
+    if 'gene2variant' in evidence:
+        try:
+            float(evidence['evidence']['gene2variant']['provenance_type']['database']['version'])
+        except:
+            evidence['evidence']['gene2variant']['provenance_type']['database']['version'] = ''
+            fixed = True
+        try:
+            float(evidence['evidence']['gene2variant']['provenance_type']['database']['dbxref']['version'])
+        except:
+            evidence['evidence']['gene2variant']['provenance_type']['database']['dbxref']['version'] = ''
+            fixed = True
+    # Split EVA in two datasources depending on the datatype
+    if (evidence['sourceID'] == 'eva') and \
+            (evidence['type'] == 'somatic_mutation'):
+        evidence['sourceID'] = 'eva_somatic'
+        fixed = True
+    # Move genetic_literature to genetic_association
+    if evidence['type'] == 'genetic_literature':
+        evidence['type'] = 'genetic_association'
+
+    if 'provenance_type' in evidence and \
+                    'database' in evidence['provenance_type'] and \
+                    'version' in evidence['provenance_type']['database']:
+        evidence['provenance_type']['database']['version'] = str(evidence['provenance_type']['database']['version'])
+
+    # Enforce eco-based score for genetic_association evidencestrings
+    if evidence['type'] == 'genetic_association':
+        available_score = None
+        eco_uri = None
+        try:
+            available_score = evidence['evidence']['gene2variant']['resource_score']['value']
+        except KeyError:
+            if 'resource_score' in evidence['evidence'] and \
+                            'value' in evidence['evidence']['resource_score']:
+                available_score = evidence['evidence']['resource_score']['value']
+        try:
+            eco_uri = evidence['evidence']['gene2variant']['functional_consequence']
+            if 'evidence_codes' in evidence['evidence']:
+                eco_uri = evidence['evidence']['evidence_codes']
+        except KeyError:
+            if 'evidence_codes' in evidence['evidence']:
+                eco_uri = evidence['evidence']['evidence_codes'][0]
+                eco_uri.rstrip()
+
+        if eco_uri in eco_scores:
+            if 'gene2variant' in evidence['evidence']:
+                if 'resource_score' not in evidence['evidence']['gene2variant']:
+                    evidence['evidence']['gene2variant']['resource_score'] = {}
+                evidence['evidence']['gene2variant']['resource_score']['value'] = eco_scores[eco_uri]
+                evidence['evidence']['gene2variant']['resource_score']['type'] = 'probability'
+                if available_score != eco_scores[eco_uri]:
+                    fixed = True
+        else:
+            print("Cannot find a score for eco code %s in evidence id %s" % (eco_uri, evidence['id']))
+
+    # Remove identifiers.org from cttv activity  and target type ids
+    if 'target_type' in evidence['target']:
+        evidence['target']['target_type'] = evidence['target']['target_type'].split('/')[-1]
+    if 'activity' in evidence['target']:
+        evidence['target']['activity'] = evidence['target']['activity'].split('/')[-1]
+
+    # Remove identifiers.org from ecos
+    new_eco_ids = []
+    if 'evidence_codes' in evidence['evidence']:
+        eco_ids = evidence['evidence']['evidence_codes']
+    elif 'variant2disease' in evidence['evidence']:
+        if 'variant2disease' in evidence['evidence']:
+            eco_ids = evidence['evidence']['variant2disease']['evidence_codes']
+        if 'gene2variant' in evidence['evidence']:
+            eco_ids.extend(evidence['evidence']['gene2variant']['evidence_codes'])
+    elif 'target2drug' in evidence['evidence']:
+        eco_ids = evidence['evidence']['target2drug']['evidence_codes']
+        eco_ids.extend(evidence['evidence']['drug2clinic']['evidence_codes'])
+    elif 'biological_model' in evidence['evidence']:
+        eco_ids = evidence['evidence']['biological_model']['evidence_codes']
+    else:
+        eco_ids = []  # something wrong here...
+    eco_ids = list(set(eco_ids))
+    for idorg_eco_uri in eco_ids:
+        code = idorg_eco_uri.strip()
+        if code is not None:
+            # if len(code.split('_')) != 2:
+            # self.logger.warning("could not recognize evidence code: %s in id %s | added anyway" %(evidence['id'],
+            # idorg_eco_uri))
+            new_eco_ids.append(code)
+    evidence['evidence']['evidence_codes'] = list(set(new_eco_ids))
+    if not new_eco_ids:
+        print("No valid ECO could be found in evidence: %s. original ECO mapping: %s" % (
+            evidence['id'], str(eco_ids)[:100]))
+
+    return Evidence(evidence), fixed
+
+if __name__ == "__main__":
+    ev_files = [
+        'affected_pathway.json',
+        'animal_model.json',
+        'genetic_association.json',
+        'known_drug.json',
+        'literature.json',
+        'rna_expression.json',
+        'somatic_mutation.json',
+        ]
+    for ev_file in ev_files:
+        with jsonlines.open(ev_file) as ev_reader:
+            for ev_obj in ev_reader:
+                ev_parsed = Evidence(ev_obj)
+                ev_fixed, fixed = fix_evidence(ev_parsed)
+                ev_fixed.score_evidence()
+                print(ev_fixed.evidence['type'],
+                      ev_fixed.evidence['target']['id'],
+                      ev_fixed.evidence['disease']['id'],
+                      ev_fixed.evidence['scores']['association_score'])
