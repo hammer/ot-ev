d5901f7c4739a656879b50dfdecdafb34d874df6
Eric Czech <eczech@Erics-MacBook-Pro-2.local>
2019-10-07 17:58:38 -0400

    Adding initial export and analysis notebooks

--- eda/data-pipeline/notebooks/utils.py
+++ eda/data-pipeline/notebooks/utils.py
@@ -1,7 +1,15 @@
 import mrtarget
 import mrtarget.cfg
 from mrtarget.common.connection import new_es_client
+from opentargets_urlzsource import URLZSource
+import yaml
 
+DATA_CONFIG_URL = 'https://storage.googleapis.com/open-targets-data-releases/19.09/input/mrtarget.data.19.09.yml'
+
+def get_data_config():
+    with URLZSource(DATA_CONFIG_URL).open() as f:
+        return yaml.safe_load(f)
+    
 def get_es_config():
     return mrtarget.cfg.get_config('/lab/repos/data_pipeline/mrtarget.es.yml')
 
